cf3517f6ba57f9368222422b92ec99329798c861
Rostyslav Okhochyi <ohochyi2002@gmail.com>
2024-05-04 17:18:13 +0300

    feat(search,filtering,ordering): added search, filtering, ordering for event application

--- drf_test_task_app/events/views.py
+++ drf_test_task_app/events/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 
 from events.models import Event
@@ -7,4 +9,8 @@ from events.serializers import EventSerializer
 class EventViewSet(ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['name']
+    search_fields = ['name', 'description']
+    ordering_fields = ['name', 'description']
 
